70b714d7ced5de3f17ef54ecd038ca9e1387d0ef
AlMirai <live.almirai@outlook.com>
2022-06-12 10:14:18 +0000

    feat(db): ✨ add base dbutil
    Feat:
    1. add BaseDbUtil as abstract dbuitl
    2. add StoredProxy
    
    Break:
    1. Protocol, Verify, Anonymous implements Enum instead of Flag

---
+++ db/base_dbutil.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
+from db.model import Anonymous, Protocol, Proxy, StoredProxy, Verify
+
+
+class BaseDbUtil(ABC):
+    @abstractmethod
+    async def try_insert(self, proxy: Proxy) -> StoredProxy | None:
+        """
+        Try to insert a :param:`proxy` into the database.
+
+        :param proxy: Proxy
+        :returns: Inserted proxy if not exists, otherwise None.
+        """
+        pass
+
+    @abstractmethod
+    async def increase_score(self, proxy: StoredProxy, step=1) -> StoredProxy | None:
+        """
+        Increase the score of :param:`proxy`.
+
+        :param proxy: Proxy stored in database.
+        :param step: Step to increment the score.
+        :returns: Updated proxy if successfully, otherwise None.
+        """
+        pass
+
+    @abstractmethod
+    async def decrease_score(self, proxy: StoredProxy, step=1) -> StoredProxy | None:
+        """
+        Decrease the score of :param:`proxy`.
+
+        :param proxy: Proxy stored in database.
+        :param step: Step to decrement the score.
+        :returns: Updated proxy if successfully, otherwise None.
+        """
+        pass
+
+    @abstractmethod
+    async def update_speed(self, proxy: StoredProxy, new_speed: float) -> StoredProxy | None:
+        """
+        Update the speed of :param:`proxy` to :param:`new_speed`.
+
+        :param proxy: Proxy stored in the database.
+        :param new_speed: New speed value.
+        :returns: Updated proxy if successfully, otherwise None.
+        """
+        pass
+
+    @abstractmethod
+    async def gets(self, protocol: Protocol = None, ip: str = None, port: int = None, verify: Verify = None, anonymous: Anonymous = None, domestic: bool = None) -> List[StoredProxy]:
+        """
+        Get proxies by given conditions.
+        """
+        pass
--- db/model.py
+++ db/model.py
@@ -1,36 +1,52 @@
-from enum import Flag, unique
+from copy import copy
+from enum import Enum, unique
 
 from util.ip_util import IpUtil
 
 
 @unique
-class Protocol(Flag):
+class Protocol(Enum):
     """Protocol types."""
-    HTTP = 1 << 0,
-    HTTPS = 1 << 1,
-    SOCKS4 = 1 << 2,
-    SOCKS5 = 1 << 3,
+    HTTP = 1
+    HTTPS = 2
+    SOCKS4 = 3
+    SOCKS5 = 4
 
 
 @unique
-class Verify(Flag):
+class Verify(Enum):
     """Proxy verify types."""
-    HTTP = 1 << 0,
-    HTTPS = 1 << 1,
-    TCP = 1 << 2,
-    UDP = 1 << 3,
+    HTTP = 1
+    HTTPS = 2
+    TCP = 3
+    UDP = 4
+
+
+@unique
+class Anonymous(Enum):
+    """Anonymous types."""
+    TRANSPARENT = 1
+    ANONYMOUS = 2
+    CONFUSE = 3
+    HIGH = 4
 
 
 class Proxy:
-    def __init__(self, protocol: Protocol, ip: str, port: int, verify: Verify):
+    def __init__(self, protocol: Protocol, ip: str, port: int, verify: Verify, anonymous: Anonymous,
+                 domestic: bool = True, address: str = '', speed: float = 0):
         self._protocol = protocol
         if not IpUtil.is_formed_ipv4(ip):
             raise ValueError("Invalid ip format")
         self._ip = ip
-        if not port or port < 1 or port > 65535:
+        if not port or int(port) < 1 or int(port) > 65535:
             raise ValueError("Invalid port, must be between 1 and 65535")
         self._port = port
         self._verify = verify
+        self._anonymous = anonymous
+        # public properties
+        self.domestic: bool = domestic or True
+        self.address: str = address or ''
+        self.speed: float = speed or 0
 
     @property
     def protocol(self):
@@ -48,15 +64,46 @@ class Proxy:
     def verify(self):
         return self._verify
 
+    @property
+    def anonymous(self):
+        return self._anonymous
+
     def __eq__(self, __o: object) -> bool:
-        if not isinstance(__o, Proxy):
+        if __o is None:
+            return False
+        if id(self) == id(__o):
+            return True
+        if not isinstance(__o, self.__class__):
             return False
         if self.protocol == __o.protocol and \
-                self.ip.string() == __o.ip.strip() and \
+                self.ip.strip() == __o.ip.strip() and \
                 self.port == __o.port and \
-                self.verify == __o.verify:
+                self.verify == __o.verify and \
+                self.anonymous == __o.anonymous:
             return True
         return False
 
     def __hash__(self) -> int:
         return hash(self.protocol.name+self.ip+self.port+self.verify.name)
+
+    def copy(self):
+        return copy(self)
+
+
+class StoredProxy(Proxy):
+    """:class:`Proxy` which stored in, add `id` and `score` properties."""
+
+    def __init__(self, id: str | int,  protocol: Protocol, ip: str, port: int, verify: Verify, score: int, anonymous: Anonymous,
+                 domestic: bool = True, address: str = '', speed: float = 0):
+        super(StoredProxy, self).__init__(
+            protocol, ip, port, verify, anonymous, domestic, address, speed)
+        self._id = id
+        self._score = score
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def score(self):
+        return self._score
