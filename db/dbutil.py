d08591a83951b9eed8a37a4f49e95aea301f1931
AlMirai <live.almirai@outlook.com>
2022-06-14 17:26:11 +0000

    feat(db): ✨ add DbUtil

---
+++ db/dbutil.py
@@ -0,0 +1,80 @@
+from al_utils.singleton import Singleton
+from config import DBConfig
+
+from db.base_dbutil import BaseDbUtil
+from db.model import Anonymous, Protocol, Proxy, StoredProxy, Verify
+from db.rdb.rdb_dbutil import RDBDbUtil
+
+
+class DbUtil(BaseDbUtil, Singleton):
+    def __init__(self):
+        self.db: BaseDbUtil
+        match DBConfig.TYPE:
+            case 'rdb':
+                self.db = RDBDbUtil()
+            case _:
+                raise ValueError(f'Unsupported db type, got {DBConfig.TYPE}')
+
+    def _any_falsy(*args: str) -> bool:
+        """
+        Determine whether at least one of :param:`args` is falsy.
+
+        :returns: False if all :param:`args` are truthy."""
+        for arg in args:
+            if not arg:
+                return True
+        return False
+
+    async def try_insert(self, proxy: Proxy) -> StoredProxy | None:
+        if self._any_falsy(proxy.protocol, proxy.ip, proxy.port):
+            raise ValueError(f"protocol, ip and port must be set, "
+                             f"but got {proxy}")
+        if not proxy.verify:
+            match proxy.protocol:
+                case Protocol.HTTP:
+                    verify = Verify.HTTP
+                case Protocol.HTTPS:
+                    verify = Verify.HTTPS
+                case Protocol.SOCKS4:
+                    verify = Verify.TCP
+                case Protocol.SOCKS5:
+                    verify = Verify.UDP
+        if not proxy.anonymous:
+            proxy.anonymous = Anonymous.TRANSPARENT
+        proxy.verify = verify
+        return await self.db.try_insert(proxy)
+
+    async def increase_score(self, proxy: StoredProxy, step=1) -> StoredProxy | None:
+        if not proxy.id:
+            raise ValueError(f"id must be set, "
+                             f"but got {proxy}")
+        if step < 1:
+            raise ValueError(f"step must ≥ 1.")
+        return await self.db.increase_score(proxy, step)
+
+    async def decrease_score(self, proxy: StoredProxy, step=1) -> StoredProxy | None:
+        if not proxy.id:
+            raise ValueError(f"id must be set, "
+                             f"but got {proxy}")
+        if step < 1:
+            raise ValueError(f"step must ≥ 1.")
+        return await self.db.decrease_score(proxy, step)
+
+    async def update_speed(self, proxy: StoredProxy, new_speed: float) -> StoredProxy | None:
+        if not proxy.id:
+            raise ValueError(f"id must be set, "
+                             f"but got {proxy}")
+        if new_speed <= 0 and new_speed != -1:
+            raise ValueError(f"speed must > 0, or -1(timeout).")
+        return await self.db.update_speed(proxy, new_speed)
+
+    async def gets(self, protocol: Protocol = None, ip: str = None, port: int = None, verify: Verify = None, anonymous: Anonymous = None, domestic: bool = None,
+                   limit: int = 100, offset: int = 0) -> list[StoredProxy]:
+        if not limit or limit < 0:
+            limit = 100
+        if not offset or offset < 0:
+            offset = 0
+        return await self.db.gets(protocol, ip, port, verify, anonymous, domestic, limit, offset)
+
+    async def count(self, protocol: Protocol = None, ip: str = None, port: int = None, verify: Verify = None, anonymous: Anonymous = None, domestic: bool = None) -> int:
+        return await self.db.count(protocol, ip, port, verify, anonymous, domestic)
