7891a9d8cc85e62fa5c07071711760c81261d168
AlMirai <live.almirai@outlook.com>
2022-06-14 08:45:11 +0000

    feat: ✨ add ValidConfig
    Feat:
    1. add ValidConfig
    2. configs add typing

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict
 
 from util.config_util import ConfigUtil
+from util.ip import PublicIP
 
 
 class RDBConfig:
@@ -14,15 +15,15 @@ class RDBConfig:
     def get(k: str, d: any):
         return ConfigUtil().get_key(f'db.rdb.{k}', f'_DB_RDB_{k.upper()}', d)
 
-    ENGINE = get('engine', 'mysql+pymysql')
-    USERNAME = get('username', 'root')
-    PASSWORD = get('password', '')
-    HOST = get('host', 'localhost')
-    PORT = str(get('port', '3306'))
-    DB = get('db', 'proxy_ip')
-    CHARSET = get('charset', 'utf8mb4')
-    URL = get('url', f'{ENGINE}://{USERNAME}:{PASSWORD}@'
-              f'{HOST}:{PORT}/{DB}?charset={CHARSET}')
+    ENGINE: str = get('engine', 'mysql+pymysql')
+    USERNAME: str = get('username', 'root')
+    PASSWORD: str = get('password', '')
+    HOST: str = get('host', 'localhost')
+    PORT: str = str(get('port', '3306'))
+    DB: str = get('db', 'proxy_ip')  # database name
+    CHARSET: str = get('charset', 'utf8mb4')
+    URL: str = get('url', f'{ENGINE}://{USERNAME}:{PASSWORD}@'
+                   f'{HOST}:{PORT}/{DB}?charset={CHARSET}')
     # other parameters
     # see more: https://docs.sqlalchemy.org/en/14/core/engines.html?highlight=create_engine#sqlalchemy.create_engine.params
     EXTRA: Dict[str, Any] = get('extra', {})
@@ -32,5 +33,13 @@ class DBConfig:
     def get(k: str, d: any):
         return ConfigUtil().get_key(f'db.{k}', f'_DB_{k.upper()}', d)
 
-    TYPE = get('type', 'rdb')
+    TYPE: str = get('type', 'rdb')
     RDB = RDBConfig
+
+
+class ValidConfig:
+    def get(k: str, d: any):
+        return ConfigUtil().get_key(f'valid.{k}', f'_VALID_{k.upper()}', d)
+
+    PUBLIC_IP: str = get('pubip', PublicIP.public_ip())
+    TIMEOUT: float = get('timeout', 5)
