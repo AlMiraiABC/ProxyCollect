0ba10b36a2233f5958c28ee4450fbdcf782eb647
AlMirai <live.almirai@outlook.com>
2022-06-14 15:10:37 +0000

    docs: 📝 add docs for config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict, TypedDict
+from typing import Dict, TypedDict
 
 from util.config_util import ConfigUtil
 from util.ip import PublicIP
@@ -16,17 +16,32 @@ class RDBConfig:
         return ConfigUtil().get_key(f'db.rdb.{k}', f'_DB_RDB_{k.upper()}', d)
 
     ENGINE: str = get('engine', 'mysql+pymysql')
+    """RDB database driver"""
     USERNAME: str = get('username', 'root')
+    """RDB login username."""
     PASSWORD: str = get('password', '')
+    """RDB login password."""
     HOST: str = get('host', 'localhost')
+    """RDB host ip address."""
     PORT: str = str(get('port', '3306'))
-    DB: str = get('db', 'proxy_ip')  # database name
+    """RDB connect port."""
+    DB: str = get('db', 'proxy_ip')
+    """Database name."""
     CHARSET: str = get('charset', 'utf8mb4')
-    URL: str = get('url', f'{ENGINE}://{USERNAME}:{PASSWORD}@'
-                   f'{HOST}:{PORT}/{DB}?charset={CHARSET}')
-    # other parameters
-    # see more: https://docs.sqlalchemy.org/en/14/core/engines.html?highlight=create_engine#sqlalchemy.create_engine.params
+    """Charset in connect."""
+    URL: str = get(
+        'url',
+        f'{ENGINE}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}?charset={CHARSET}'
+    )
+    """Connect url."""
     EXTRA: Dict[str, any] = get('extra', {})
+    """
+    Other parameters
+
+    See:
+    ---------
+    https://docs.sqlalchemy.org/en/14/core/engines.html?highlight=create_engine#sqlalchemy.create_engine.params
+    """
 
 
 class DBConfig:
@@ -34,6 +49,7 @@ class DBConfig:
         return ConfigUtil().get_key(f'db.{k}', f'_DB_{k.upper()}', d)
 
     TYPE: str = get('type', 'rdb')
+    """Database type."""
     RDB = RDBConfig
 
 
@@ -42,21 +58,32 @@ class ValidConfig:
         return ConfigUtil().get_key(f'valid.{k}', f'_VALID_{k.upper()}', d)
 
     PUBLIC_IP: str = get('pubip', PublicIP.public_ip())
-    TIMEOUT: float = get('timeout', 5)
+    """Public ip address of host."""
+    TIMEOUT: float = float(get('timeout', 5))
+    """Time seconds to wait for connection."""
 
 
 class CrawlDetailConfig(TypedDict):
+    """The detail config of each type of crawl."""
     headers: dict[str, str]
+    """Request headers."""
     semaphore: int
+    """The maximum number of concurrent."""
     timeout: int
+    """Time seconds to wait for connection."""
 
 
 class CrawlConfig:
+    """Crawls common configs. Priority lower than :class:`CrawlDetailConfig`"""
     def get(k: str, d: any):
         return ConfigUtil().get_key(f'valid.{k}', f'_CRAWL_{k.upper()}', d)
 
-    DETAIL: Callable[[str, CrawlDetailConfig],
-                     CrawlDetailConfig] = lambda k, d: CrawlConfig.get(k, d)
+    def DETAIL(k: CrawlDetailConfig, d: CrawlDetailConfig):
+        """Get type of :param:`k`'s detail config."""
+        return CrawlConfig.get(k, d)
     HEADERS: dict[str, str] = get('headers', {})
-    SEMAPHORE: int = get('semaphore', 10)
-    TIMEOUT: int = get('timeout', 10)
+    """Common headers."""
+    SEMAPHORE: int = int(get('semaphore', 10))
+    """The maximum number of concurrent."""
+    TIMEOUT: int = int(get('timeout', 10))
+    """Time seconds to wait for connection."""
