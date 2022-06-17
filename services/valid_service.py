84d9dec4a16881e013eac61664607d741cd27cca
AlMirai <live.almirai@outlook.com>
2022-06-17 02:45:50 +0000

    feat(services): ✨ test valid service
    Feat:
    1. run add param patch_cb
    2. run return (count_len, fails_len, timeouts_len)
    3. add _count to calc lens.
    
    Break:
    1. ValidService.__init__ remove param loop(no loger support)
    2. valid_proxies remove params and get proxies from _queue
    3. simplify run_patch
    
    Fix:
    1. get_patch cannot set _cursor to -1

--- services/valid_service.py
+++ services/valid_service.py
@@ -1,7 +1,8 @@
 import asyncio
 import time
-from asyncio import AbstractEventLoop, Queue, Semaphore
+from asyncio import Queue
 from multiprocessing import Event
+from typing import Callable
 
 from aiohttp import ClientSession
 from al_utils.logger import Logger
@@ -13,8 +14,8 @@ logger = Logger(__file__).logger
 
 
 class ValidService:
-    def __init__(self, patch: int = 500, semaphore: int = 50, loop: AbstractEventLoop = None):
-        self.patch = patch if patch > 50 else 500
+    def __init__(self, patch: int = 500, semaphore: int = 50):
+        self.patch = patch if patch >= 50 else 500
         self.semaphore = semaphore if semaphore > 10 else 50
         self._cursor: int = 0
         """
@@ -24,14 +25,15 @@ class ValidService:
         self._db = DbUtil()
         self._valid = Valid()
         self._stop = Event()
-        self._queue: Queue[StoredProxy] = Queue(patch*1.2, loop=loop)
+        self._queue: Queue[StoredProxy] = Queue(patch*1.2)
 
     async def get_patch(self) -> list[StoredProxy]:
         """Get next patch proxies."""
         proxies = await self._db.gets(limit=self.patch, offset=self._cursor)
         if not proxies:
             self._cursor = -1
-        self._cursor += self.patch
+        else:
+            self._cursor += len(proxies)
         return proxies
 
     async def update(self, session: ClientSession, proxy: StoredProxy):
@@ -48,15 +50,21 @@ class ValidService:
                            f'{{anonymous: {anonymous}, speed: {speed}}}')
         return ret
 
-    async def valid_proxies(self, proxies: list[StoredProxy]):
-        ret: list[StoredProxy] = []
+    async def valid_proxies(self):
+        """
+        Get proxies from queue and valid it.
+
+        Updated proxy will put in `rqueue`.
+        """
+        updated: list[StoredProxy] = []
         async with ClientSession() as session:
             async with asyncio.Semaphore(self.semaphore):
-                for proxy in proxies:
-                    ret.append(await self.update(session, proxy))
-        return ret
+                while not self._queue.empty() and not self._stop.is_set():
+                    proxy = await self._queue.get()
+                    updated.append(await self.update(session, proxy))
+        return updated
 
-    async def run_patch(self) -> list[StoredProxy]:
+    async def run_patch(self):
         """
         Run one patch.
 
@@ -65,24 +73,20 @@ class ValidService:
         proxies = await self.get_patch()
         for proxy in proxies:
             await self._queue.put(proxy)
-        ret = []
-        async with ClientSession() as session:
-            async with Semaphore(self.semaphore):
-                while not self._queue.empty() and not self._stop.is_set():
-                    proxy = await self._queue.get()
-                    ret.append(self.update(session, proxy))
-        return ret
+        return self.valid_proxies()
 
-    async def run(self):
+    async def run(self, patch_cb: Callable[[list[StoredProxy]], None] = None):
         """
         Start or continue to run valid service.
 
         Valid all proxies in one ioloop. This is IO bound tasks and multiprocessing will not
         imporve efficiency.
         """
+        patch_cb = patch_cb or (lambda *_: None)
         if self._cursor < 0:
             raise RuntimeError('Service already stopped.')
         logger.info(f"Running valid service with {self.max_workers} workers.")
+        count, fails, timeouts = 0, 0, 0
         self._stop.clear()
         while self._cursor >= 0 and not self._stop.is_set():
             logger.info(f"Get patch from {self._cursor}.")
@@ -90,13 +94,28 @@ class ValidService:
             results = await self.run_patch()
             logger.info(f"The patch {self._cursor}-{self._cursor+self.patch} finished "
                         f"in {time.time()-start_time} seconds.")
-            count = len(results)
-            fails = [result for result in results if not result]
-            timeouts = [result for result in results
-                        if result and result.speed == -1]
-            logger.info(f"The The patch {self._cursor}-{self._cursor+self.patch} valid {count} proxies, "
-                        f"{len(fails)} failed and"
-                        f"{len(timeouts)} timed out.")
+            c, f, t = self._count(results)
+            logger.info(f"The The patch {self._cursor}-{self._cursor+self.patch} valid {c} proxies, "
+                        f"{f} failed and"
+                        f"{t} timed out.")
+            count += c
+            fails += f
+            timeouts += t
+        self._stop.set()
+        return count, fails, timeouts
+
+    def _count(self, proxies: list[StoredProxy]):
+        """
+        Reduce updated proxies.
+
+        :param updated: Updated proxies.
+        :returns: len(proxies), len(proxy is None), len(proxy.speed is -1)
+        """
+        c = len(proxies)
+        f = len([proxy for proxy in proxies if not proxy])
+        t = len([proxy for proxy in proxies
+                 if proxy and proxy.speed == -1])
+        return c, f, t
 
     async def pause(self):
         """
---
+++ tests/services_test/valid_service_test.py
@@ -0,0 +1,95 @@
+from unittest import IsolatedAsyncioTestCase, skip
+from unittest.mock import patch
+
+from db.dbutil import DbUtil
+from db.model import Anonymous, Protocol, StoredProxy, Verify
+from services.valid_service import ValidService
+from util.valid import Valid
+
+
+async def mock_proxy(anonymous=Anonymous.HIGH, speed=1.0):
+    return StoredProxy(1, Protocol.HTTP, '127.0.0.1',
+                       33, Verify, 100, anonymous, speed=speed)
+
+
+async def mock_proxies(num=55):
+    proxy = StoredProxy(1, Protocol.HTTP, '127.0.0.1',
+                        33, Verify, 100, Anonymous.HIGH)
+    return [proxy for _ in range(num)]
+
+
+async def mock_async(*args):
+    return args
+
+
+@patch.object(DbUtil, '__init__', lambda *_, **__: None)
+@patch.object(Valid, '__init__', lambda *_, **__: None)
+class TestValidService(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.service = ValidService()
+
+    @patch.object(DbUtil, 'gets', lambda *_, **__: mock_proxies(5))
+    async def test_get_patch(self):
+        proxies = await self.service.get_patch()
+        self.assertEqual(self.service._cursor, 5)
+        self.assertEqual(len(proxies), 5)
+
+    @patch.object(DbUtil, 'gets', lambda *_, **__: mock_proxies(0))
+    async def test_get_patch_empty(self):
+        proxies = await self.service.get_patch()
+        self.assertEqual(self.service._cursor, -1)
+        self.assertEqual(len(proxies), 0)
+
+    @skip('return self._db._update(...)')
+    @patch.object(Valid, 'async_req', lambda *_, **__: mock_async())
+    @patch.object(DbUtil, '_update', lambda *_: mock_proxy())
+    async def test_update(self):
+        pass
+
+    @patch.object(ValidService, 'update', lambda *_, **__: mock_proxy())
+    async def test_valid_proxies(self):
+        S = 5
+        for i in range(S):
+            await self.service._queue.put(i)
+        updated = await self.service.valid_proxies()
+        self.assertEqual(len(updated), S)
+
+    async def test_valid_proxies_empty(self):
+        updated = await self.service.valid_proxies()
+        self.assertListEqual(updated, [])
+
+    @patch.object(ValidService, 'valid_proxies', lambda *_, **__: mock_async(None))
+    @patch.object(ValidService, 'get_patch', lambda *_, **__: mock_proxies(5))
+    async def test_run_patch(self):
+        await self.service.run_patch()
+        self.assertEqual(self.service._queue.qsize(), 5)
+
+    @patch.object(ValidService, 'get_patch', lambda *_, **__: mock_proxies(0))
+    async def test_run_patch_empty(self):
+        await self.service.run_patch()
+        self.assertEqual(self.service._queue.qsize(), 0)
+
+    async def test__count(self):
+        proxies = []
+        fails = [None]*10
+        proxies.extend(fails)
+        timeouts = [await mock_proxy(speed=-1)]*5
+        proxies.extend(timeouts)
+        sucs = [await mock_proxy()]*3
+        proxies.extend(sucs)
+        c, f, t = self.service._count(proxies)
+        self.assertEqual(len(proxies), c)
+        self.assertEqual(len(fails), f)
+        self.assertEqual(len(timeouts), t)
+
+    @skip("How to test")
+    async def test_pause(self):
+        pass
+
+    @skip("How to test")
+    async def test_stop(self):
+        pass
+
+    @skip("")
+    async def test_run(self):
+        pass
